2a1acc36e3d026c62d20d587c6601d27a3e7c3f7
Rahul <89585019+Rahulgoswami021@users.noreply.github.com>
2021-10-29 14:43:33 +0530

    Update number.js

--- Day-009/number.js
+++ Day-009/number.js
@@ -3,26 +3,30 @@ const strat = document.getElementById("btns");
 const input = document.getElementById("text");
 const msg1 = document.getElementsByClassName("congrets");
 const msg2 = document.getElementsByClassName("steps");
-let count  =0;
 
 strat.addEventListener('click',(e)=>{
     const randNumber =Math.floor( Math.random()*100+1);
 console.log(randNumber);
+let count =0;
+msg1[0].textContent="";
+msg2[0].textContent="";
 strat.innerText ="Re-start"
-game(randNumber);
+game(randNumber, count);
 
 })
 
   
-function game(randNumber){
+function game(randNumber,count){
     btn.addEventListener('click',(e)=>{
         e.preventDefault();
         if(input.value > randNumber){
             count++;
-            msg1[0].textContent=" Too Height !, Enter a less number"
+            msg1[0].textContent=" Too Height !, Enter a less number";
+            msg2[0].textContent="";
         }else if(input.value < randNumber){
             count++;
             msg1[0].textContent=" Too low ! , Enter a greater number"
+            msg2[0].textContent="";
         }else{
             count++;
             msg1[0].textContent = " You Find the number";
